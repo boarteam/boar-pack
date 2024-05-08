883b9a7e2e87eb4959d0c8797478632b3583e434
proxcentb <proxcentb@gmail.com>
2024-05-08 14:42:24 +0300

    fixes after rebase

--- packages/liquidity-manager-backend/src/modules/ecn-connect-schema/dto/ecn-connect-schema-create.dto.ts
+++ packages/liquidity-manager-backend/src/modules/ecn-connect-schema/dto/ecn-connect-schema-create.dto.ts
@@ -2,10 +2,10 @@ import Joi from 'joi';
 import { JoiSchema } from 'nestjs-joi';
 
 export class EcnConnectSchemaCreateDto {
-  @JoiSchema(Joi.number().integer().positive().required())
+  @JoiSchema(Joi.number().integer().min(0).required())
   fromModuleId: number;
 
-  @JoiSchema(Joi.number().integer().positive().required())
+  @JoiSchema(Joi.number().integer().min(0).required())
   toModuleId: number;
 
   @JoiSchema(Joi.number().integer().min(0).max(1).optional())
--- packages/liquidity-manager-backend/src/modules/ecn-connect-schema/dto/ecn-connect-schema-update.dto.ts
+++ packages/liquidity-manager-backend/src/modules/ecn-connect-schema/dto/ecn-connect-schema-update.dto.ts
@@ -2,10 +2,10 @@ import Joi from 'joi';
 import { JoiSchema } from 'nestjs-joi';
 
 export class EcnConnectSchemaUpdateDto {
-  @JoiSchema(Joi.number().integer().positive().optional())
+  @JoiSchema(Joi.number().integer().min(0).optional())
   fromModuleId?: number;
 
-  @JoiSchema(Joi.number().integer().positive().optional())
+  @JoiSchema(Joi.number().integer().min(0).optional())
   toModuleId?: number;
 
   @JoiSchema(Joi.number().integer().min(0).max(1).optional())
