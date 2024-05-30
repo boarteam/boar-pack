f3b3ca54503714d8d7fe60be8e98bbf56915dae2
Andrew Balakirev <balakirev.andrey@gmail.com>
2024-05-30 19:09:35 +0200

    Build process removed

--- packages/liquidity-manager-frontend/src/routes.ts
+++ packages/liquidity-manager-frontend/src/routes.ts
@@ -4,7 +4,7 @@ const routes = () => ({
   icon: 'DollarCircleOutlined',
   access: 'canViewLiquidity',
   layout: 'top',
-  component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/dist/pages/LiquidityManagerWrapper',
+  component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/src/pages/LiquidityManagerWrapper',
   routes: [
     {
       path: '/liquidity',
@@ -13,47 +13,47 @@ const routes = () => ({
     {
       path: '/liquidity/ecn-modules',
       name: 'ecn-modules',
-      component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/dist/pages/EcnModules',
+      component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/src/pages/EcnModules',
     },
     {
       path: '/liquidity/ecn-modules/:id',
       name: 'ecn-module',
       hideInMenu: true,
-      component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/dist/pages/EcnModule',
+      component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/src/pages/EcnModule',
     },
     {
       path: '/liquidity/ecn-setups/:id',
       name: 'ecn-setup',
       hideInMenu: true,
-      component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/dist/pages/EcnSetup',
+      component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/src/pages/EcnSetup',
     },
     {
       path: '/liquidity/ecn-connect-schemas/:connectSchemaId/subscription-schemas/:hash',
       name: 'ecn-subscription-schema',
       hideInMenu: true,
-      component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/dist/pages/EcnConnectSchemas/EcnSubscrSchema',
+      component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/src/pages/EcnConnectSchemas/EcnSubscrSchema',
     },
     {
       path: '/liquidity/ecn-instruments',
       name: 'ecn-instruments',
-      component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/dist/pages/EcnInstruments',
+      component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/src/pages/EcnInstruments',
     },
     {
       path: '/liquidity/ecn-instruments/:hash',
       hideInMenu: true,
       name: 'ecn-instrument',
-      component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/dist/pages/EcnInstruments/EcnInstrument',
+      component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/src/pages/EcnInstruments/EcnInstrument',
     },
     {
       path: '/liquidity/users-inst',
       name: 'users-inst',
-      component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/dist/pages/UsersInst',
+      component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/src/pages/UsersInst',
     },
     {
       path: '/liquidity/users-inst/:id',
       name: 'user-inst',
       hideInMenu: true,
-      component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/dist/pages/UsersInst/UserInst',
+      component: '@/../node_modules/@jifeon/boar-pack-liquidity-manager-frontend/src/pages/UsersInst/UserInst',
     }
   ]
 });
