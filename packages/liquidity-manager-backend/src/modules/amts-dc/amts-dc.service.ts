de67c3fdbdda5d21f6bd14924a40b0d973b027cd
Andrew Balakirev <balakirev.andrey@gmail.com>
2024-06-19 11:12:32 +0200

    @feat(liquidity-manager-backend): ADM-107 AMTS DC Module

---
+++ packages/liquidity-manager-backend/src/modules/amts-dc/amts-dc.service.ts
@@ -0,0 +1,156 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { HttpService } from "@nestjs/axios";
+import crypto from 'crypto';
+import { LosslessNumber, parse } from "lossless-json";
+import {
+  MTAttachStreamRequest,
+  MTInstrumentListRequest,
+  MTInstrumentListResult,
+  MTLoginRequest,
+  MTLoginResult,
+  MTResponse
+} from "./dto/amts-dc.dto";
+import WebSocket from "ws";
+
+@Injectable()
+export class AmtsDcService {
+  private readonly logger = new Logger(AmtsDcService.name);
+  private readonly VERSION = 10;
+
+  constructor(
+    private httpService: HttpService,
+  ) {
+  }
+
+  public async request<TReq extends { method: string }, TRes>(url: string, params: TReq): Promise<MTResponse<TRes>['result']> {
+    this.logger.log(`Request to ${url}, method: ${params.method}`);
+    const response = await this.httpService.axiosRef.post<MTResponse<TRes>>(url, params, {
+      transformResponse: (data: any) => {
+        return parse(data);
+      }
+    });
+
+    const result = response.data.result;
+    if (result.status === false) {
+      this.logger.error(`Error while requesting ${url}, method: ${params.method}`);
+      this.logger.error(result);
+      throw new Error(result.description);
+    }
+
+    return response.data.result;
+  }
+
+  public async auth(url: string, params: Omit<MTLoginRequest, 'method'>) {
+    return this.request<MTLoginRequest, MTLoginResult>(url, {
+      method: 'req_login',
+      version: this.VERSION,
+      ...params,
+    });
+  }
+
+
+  private calculateSecret(data: Record<string, string | number | string[]>, sessionId: LosslessNumber, pin: LosslessNumber): string {
+    const sortedKeys = Object.keys(data).sort();
+    const keyValueString = sortedKeys.map(key => {
+      let value = data[key];
+      if (Array.isArray(value)) {
+        value = value.join('');
+      }
+      return `${key}=${value}`;
+    }).join('');
+
+    const hashingString = keyValueString
+      + sessionId.toString()
+      + pin.toString();
+
+    return crypto
+      .createHash('sha1')
+      .update(hashingString)
+      .digest('hex');
+  }
+
+  public async getInstruments(url: string, auth: MTLoginResult) {
+    const params = {
+      method: 'req_instrument_list',
+      version: this.VERSION,
+      session_id: Number(auth.session_id),
+    } as const;
+
+    return this.request<MTInstrumentListRequest, MTInstrumentListResult>(url, {
+      ...params,
+      secret: this.calculateSecret(params, auth.session_id, auth.pin),
+    });
+  }
+
+  public checkStreamConnection(url: string): Promise<void> {
+    this.logger.log(`Checking WS connection to ${url}`);
+    return new Promise((resolve, reject) => {
+      const ws = new WebSocket(url);
+      ws.on('open', () => {
+        resolve();
+        ws.close();
+      });
+
+      ws.on('error', (e) => {
+        this.logger.error(`Error on WS connection to ${url}`);
+        this.logger.error(e);
+        reject(e);
+        ws.close();
+      });
+
+      ws.on('close', () => {
+        reject(new Error('Connection closed'));
+      });
+    });
+  }
+
+  public getQuotesWebsocket({
+    url,
+    auth,
+    instruments,
+    options,
+  }: {
+    url: string,
+    auth: MTLoginResult,
+    instruments: string[],
+    options?: Partial<MTAttachStreamRequest>
+  }): WebSocket {
+    const params: MTAttachStreamRequest = {
+      method: 'attach_stream',
+      version: this.VERSION,
+      session_id: Number(auth.session_id),
+      req_id: 1,
+      subscribe_quotes: instruments,
+      quotes_timeout: 1000,
+      ...options,
+    };
+
+    const data = {
+      ...params,
+      secret: this.calculateSecret(params, auth.session_id, auth.pin),
+    };
+
+    this.logger.log(`Attaching stream to ${url}`);
+    const ws = new WebSocket(url);
+
+    ws.on('error', (e) => {
+      this.logger.error(`Error on WS connection to ${url}`);
+      this.logger.error(e);
+      ws.close();
+    });
+
+    ws.on('open', () => {
+      this.logger.log(`Sending attach_stream request to ${url}`);
+      ws.send(JSON.stringify(data), (err) => {
+        if (err) {
+          this.logger.error(`Error sending attach_stream request to ${url}`);
+          this.logger.error(err);
+          ws.close();
+        }
+      });
+    });
+
+    return ws;
+  }
+}
+
---
+++ packages/liquidity-manager-backend/src/modules/amts-dc/dto/amts-dc.dto.ts
@@ -0,0 +1,111 @@
+import { MTInstrument } from "./amts-dc.types";
+import { LosslessNumber } from "lossless-json";
+
+type LosslessJsonValue<T> = T extends number
+  ? LosslessNumber
+  : T;
+
+type LosslessJsonResult<T> = {
+  [K in keyof T]: LosslessJsonValue<T[K]>;
+};
+
+export class MTResponse<T> {
+  result: T & ({
+    status: true;
+  } | {
+    status: false;
+    description: string;
+    code: LosslessNumber;
+  });
+  version: number = 1;
+}
+
+export type MTLoginRequest = {
+  method: 'req_login';
+  login: number;
+  platform_id?: number;
+  password?: string;
+  token?: string;
+  version?: number;
+}
+
+export type MTLoginResult = LosslessJsonResult<{
+  daylight: boolean;
+  pin: number;
+  session_id: number;
+  timezone: number;
+  timeserver: string;
+  volume_div: number;
+  td: 0 | 1;
+  aes_key_b64: string;
+  aes_iv_b64: string;
+  token?: string;
+  token_lifetime?: number;
+}>;
+
+export type MTInstrumentListRequest = {
+  method: 'req_instrument_list';
+  session_id?: number;
+  secret?: string;
+  token?: string;
+  version?: number;
+}
+
+export type MTInstrumentListShortRequest = {
+  method: 'req_instrument_list_short';
+  session_id?: number;
+  secret?: string;
+  token?: string;
+  version?: number;
+}
+
+export type MTInstrumentListResult = LosslessJsonResult<{
+  instruments: MTInstrument[];
+}>;
+
+export type MTAttachStreamRequest = {
+  method: 'attach_stream';
+  req_id: number;
+  session_id?: number;
+  secret?: string;
+  market_depth?: number;
+  token?: string;
+  subscribe_quotes: string[];
+  quotes_timeout: number;
+  version?: number;
+  platform_id?: number;
+}
+
+export type MTAttachStreamResult = LosslessJsonResult<{
+  req_id: number;
+  status: boolean;
+}>;
+
+export type MTWSMessage =
+  | MTQuoteWSMessage
+  | MTInstrumentAddMessage
+  | MTInstrumentUpdateMessage
+  | MTInstrumentDeleteMessage;
+
+export type MTQuoteWSMessage = {
+  quote: {
+    bands: {
+      prices: number[];
+      volumes: number[];
+    }
+    instrument: string;
+    ts_msc: number;
+  }
+}
+
+export type MTInstrumentAddMessage = {
+  symbol_add: MTInstrument;
+}
+
+export type MTInstrumentUpdateMessage = {
+  symbol_update: MTInstrument;
+}
+
+export type MTInstrumentDeleteMessage = {
+  symbol_delete: MTInstrument;
+}
