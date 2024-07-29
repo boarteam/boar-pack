d4438e28aa80a6be0b862d13c7bcfd1f379471df
Andrew Balakirev <balakirev.andrey@gmail.com>
2024-07-29 22:48:27 +0200

    ADM-122 Real time data module

--- packages/liquidity-manager-backend/src/modules/real-time-data-app/real-time-data-app.module.ts
+++ packages/liquidity-manager-backend/src/modules/real-time-data-app/real-time-data-app.module.ts
@@ -1,24 +1,27 @@
-import { DynamicModule, Module } from "@nestjs/common";
+import { DynamicModule, Logger, Module, Optional } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { JoiPipeModule } from "nestjs-joi";
 import { CaslModule, JwtAuthModule } from "@jifeon/boar-pack-users-backend";
 import { CaslPermissionsModule } from "../casl-permissions";
 import { QuotesModule } from "../quotes/quotes.module";
 import { LiquidityManagersModule } from "../liquidity-managers";
-import { ClusterModule } from "@jifeon/boar-pack-common-backend";
-import { AMTS_DB_NAME, QuotesAppConfig } from "./quotes-app.config";
+import { ClusterModule, ClusterService } from "@jifeon/boar-pack-common-backend";
+import { AMTS_DB_NAME, RealTimeDataAppConfig } from "./real-time-data-app.config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { JwtAuthModule as LMJwtAuthModule } from '../jwt-auth/jwt-auth.module';
+import { RealTimeDataAppCluster } from "./real-time-data-app.cluster";
 
 @Module({})
-export class QuotesAppModule {
+export class RealTimeDataAppModule {
+  private readonly logger = new Logger('RealTimeDataAppModule');
+
   static forRoot(
     config: {
       dataSourceName: string;
     }
   ): DynamicModule {
     return {
-      module: QuotesAppModule,
+      module: RealTimeDataAppModule,
       imports: [
         ConfigModule,
         // TypeOrmModule.forRootAsync({
@@ -28,7 +31,7 @@ export class QuotesAppModule {
         //     ClusterModule,
         //     LiquidityManagersModule.register({ dataSourceName: config.dataSourceName }),
         //   ],
-        //   useClass: QuotesAppConfig,
+        //   useClass: RealTimeDataAppConfig,
         // }),
         JwtAuthModule.register({
           dataSourceName: config.dataSourceName,
@@ -43,12 +46,11 @@ export class QuotesAppModule {
     }
   }
 
-
   static forManagerPanel(config: {
     dataSourceName: string;
   }): DynamicModule {
     return {
-      module: QuotesAppModule,
+      module: RealTimeDataAppModule,
       imports: [
         ConfigModule,
         TypeOrmModule.forRootAsync({
@@ -58,7 +60,7 @@ export class QuotesAppModule {
             ClusterModule,
             LiquidityManagersModule.forConfig({ dataSourceName: config.dataSourceName }),
           ],
-          useClass: QuotesAppConfig,
+          useClass: RealTimeDataAppConfig,
         }),
         LMJwtAuthModule.forRoot(),
         CaslModule.forRoot(),
@@ -68,6 +70,31 @@ export class QuotesAppModule {
       ],
       providers: [],
       exports: [],
+    }
+  }
+
+  static forClusterMaster() {
+    return {
+      module: RealTimeDataAppModule,
+      imports: [
+        ClusterModule,
+      ],
+      providers: [
+        RealTimeDataAppCluster,
+      ],
+      exports: [],
+      controllers: []
+    }
+  }
+
+  constructor(
+    @Optional() private readonly cluster: ClusterService,
+    @Optional() private readonly realTimeDataCluster: RealTimeDataAppCluster,
+  ) {
+    if (this.cluster && this.realTimeDataCluster) {
+      this.cluster.addCluster(this.realTimeDataCluster);
+    } else {
+      this.logger.log('ClusterService or RealTimeDataAppCluster is not provided, RealTimeDataAppCluster will not be added to the cluster');
     }
   }
 }
