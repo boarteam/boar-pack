93d3e84bfe892e27728112ef4bff3213cdc39e6a
Andrew Balakirev <balakirev.andrey@gmail.com>
2024-07-30 11:20:10 +0200

    ADM-122 WSAuth fixed

--- packages/users-backend/src/ws-auth/ws-auth.gateway.ts
+++ packages/users-backend/src/ws-auth/ws-auth.gateway.ts
@@ -1,79 +1,25 @@
 import { Logger } from '@nestjs/common';
 import { WebSocket } from "ws";
-import { TUser } from "../users/entities/user.entity";
 import { IncomingMessage } from "http";
-import passport from "passport";
-import { JWT_AUTH } from "../auth/auth-strategies.constants";
 import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from "@nestjs/websockets";
-import { Interval } from "@nestjs/schedule";
+import { AuthSocket, WsAuthService } from "./ws-auth.service";
 
-export interface AuthSocket extends WebSocket {
-  user?: TUser;
-}
-
-@WebSocketGateway()
+@WebSocketGateway({
+  path: '/quotes',
+})
 export class WsAuthGateway implements OnGatewayConnection, OnGatewayDisconnect {
   private logger = new Logger(WsAuthGateway.name);
-  private socketsAuthenticators = new WeakMap<WebSocket, Promise<TUser | null>>();
 
-  private clients: Set<WebSocket> = new Set();
-  private clientsToTerminate: Set<WebSocket> = new Set();
+  constructor(
+    private readonly wsAuthService: WsAuthService,
+  ) {}
 
   public handleConnection(socket: AuthSocket, req: IncomingMessage) {
     this.logger.debug(`Client connected`);
-
-    this.socketsAuthenticators.set(socket, new Promise<TUser | null>((resolve) => {
-      passport.authenticate(JWT_AUTH, (err: Error, user: TUser) => {
-        if (err) {
-          this.logger.error(err, err.stack);
-          return resolve(null);
-        }
-        if (!user) {
-          return resolve(null);
-        }
-
-        socket.user = user;
-        this.addClient(socket);
-        resolve(user);
-      })(req, null, (e: Error | null) => {
-        if (e) {
-          this.logger.error(e, e.stack);
-          return resolve(null);
-        }
-      });
-    }));
-  }
-
-  public async finishInitialization(socket: WebSocket): Promise<TUser | null> {
-    return await this.socketsAuthenticators.get(socket) || null;
+    this.wsAuthService.handleConnection(socket, req);
   }
 
   public handleDisconnect(client: WebSocket) {
-    this.clients.delete(client);
+    this.wsAuthService.handleDisconnect(client);
   }
-
-  private addClient(client: WebSocket) {
-    if (this.clients.has(client)) {
-      return;
-    }
-
-    this.clients.add(client);
-    client.on('pong', () => {
-      this.clientsToTerminate.delete(client);
-    });
-  }
-
-  @Interval(5000)
-  private checkClients() {
-    this.clients.forEach((client) => {
-      if (this.clientsToTerminate.has(client)) {
-        client.terminate();
-        this.clients.delete(client);
-      } else {
-        this.clientsToTerminate.add(client);
-        client.ping();
-      }
-    });
-  }
-
 }
--- packages/users-backend/src/ws-auth/ws-auth.guard.ts
+++ packages/users-backend/src/ws-auth/ws-auth.guard.ts
@@ -1,13 +1,14 @@
 import { CanActivate, ExecutionContext, Injectable, Logger } from '@nestjs/common';
-import { WsAuthGateway } from './ws-auth.gateway';
 import { WebSocket } from "ws";
+import { WsAuthService } from "./ws-auth.service";
 
 @Injectable()
 export class WsAuthGuard implements CanActivate {
   private readonly logger = new Logger(WsAuthGuard.name);
 
-  constructor(private readonly wsAuthService: WsAuthGateway) {
-  }
+  constructor(
+    private readonly wsAuthService: WsAuthService,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const client = context.switchToWs().getClient<WebSocket>();
