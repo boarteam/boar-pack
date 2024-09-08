13c143d86802b26c0d1d7070611d630ec84b87b5
Denis Volper <denisvolper@gmail.com>
2024-09-08 15:49:00 +0300

    ADM-159 Added api client tool

---
+++ packages/users-frontend/src/tools/api-client/generated/models/EventLogCreateDto.ts
@@ -0,0 +1,39 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type EventLogCreateDto = {
+    logType: EventLogCreateDto.logType;
+    logLevel: EventLogCreateDto.logLevel;
+    action: string;
+    method?: string;
+    userId?: string;
+    userRole: string;
+    entity: string;
+    entityId?: string;
+    payload?: Record<string, any>;
+    url?: string;
+    ipAddress?: string;
+    userAgent?: string;
+    duration?: number;
+    statusCode?: number;
+};
+
+export namespace EventLogCreateDto {
+
+    export enum logType {
+        AUDIT = 'Audit',
+        OPERATIONAL = 'Operational',
+        APPLICATION = 'Application',
+    }
+
+    export enum logLevel {
+        INFO = 'Info',
+        WARNING = 'Warning',
+        ERROR = 'Error',
+    }
+
+
+}
+
