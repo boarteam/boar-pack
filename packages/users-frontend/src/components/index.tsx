2c35e7c8c733612e394d8aed563e2cbbb325d564
Andrew Balakirev <balakirev.andrey@gmail.com>
2024-10-16 21:37:36 +0200

    @feat(users-frontend): Fixed types for event logs

---
+++ packages/users-frontend/src/components/index.tsx
@@ -0,0 +1,7 @@
+export * from './EventLogs/EventLogExplanation';
+export * from './EventLogs/eventLogsSearchableColumns';
+export * from './EventLogs/EventLogsTable';
+export * from './EventLogs/EventLogsTimeline';
+export * from './EventLogs/getBrowserIcon';
+export * from './EventLogs/useEventLogsColumns';
+export {default as UserAgentDisplay} from './EventLogs/UserAgentDisplay';
