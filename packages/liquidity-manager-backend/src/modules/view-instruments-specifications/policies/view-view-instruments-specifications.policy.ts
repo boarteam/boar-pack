28c10dedf95978bab704b806888a742440e5a512
Andrew Balakirev <balakirev.andrey@gmail.com>
2024-10-28 11:45:27 +0100

    Permission for view instrument specifications

--- packages/liquidity-manager-backend/src/modules/view-instruments-specifications/policies/view-view-instruments-specifications.policy.ts
+++ packages/liquidity-manager-backend/src/modules/view-instruments-specifications/policies/view-view-instruments-specifications.policy.ts
@@ -1,7 +1,5 @@
 import { ViewInstrumentsSpecification as ViewInstrumentsSpecificationEntity } from "../entities/view-instruments-specifications.entity";
 import { Action, AppAbility, IPolicyHandler } from "@jifeon/boar-pack-users-backend";
-export const ViewInstrumentsSpecificationSubject = 'ViewInstrumentsSpecificationSubject' as const;
-
 export class ViewViewInstrumentsSpecificationsPolicy implements IPolicyHandler {
   handle(ability: AppAbility) {
     return ability.can(Action.Read, ViewInstrumentsSpecificationEntity);
--- packages/liquidity-manager-backend/src/modules/view-instruments-specifications/view-instruments-specifications.service.ts
+++ packages/liquidity-manager-backend/src/modules/view-instruments-specifications/view-instruments-specifications.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { ViewInstrumentsSpecification } from './entities/view-instruments-specifications.entity';
+import { InjectRepository } from "@nestjs/typeorm";
+import { TP_DB_NAME } from "../liquidity-app/liquidity-app.config";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class ViewInstrumentsSpecificationsService extends TypeOrmCrudService<ViewInstrumentsSpecification> {}
+export class ViewInstrumentsSpecificationsService extends TypeOrmCrudService<ViewInstrumentsSpecification> {
+  constructor(
+    @InjectRepository(ViewInstrumentsSpecification, TP_DB_NAME)
+    readonly repo: Repository<ViewInstrumentsSpecification>,
+  ) {
+    super(repo);
+  }
+}
