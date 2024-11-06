877f3e081063e5f025cd66e42d076a3de3256c2f
proxcentb <proxcentb@gmail.com>
2024-11-06 20:42:56 +0300

    ADM-208 apply sort for other entities

--- packages/liquidity-manager-backend/src/modules/sublogin-settings/sublogins-settings.service.ts
+++ packages/liquidity-manager-backend/src/modules/sublogin-settings/sublogins-settings.service.ts
@@ -6,6 +6,7 @@ import { Repository } from 'typeorm';
 import { AMTS_DB_NAME } from "../liquidity-app/liquidity-app.config";
 import { ParsedRequestParams } from "@nestjsx/crud-request";
 import { CrudRequestOptions } from "@nestjsx/crud";
+import { updateBuilderSort } from '../../tools/instrumentSort';
 
 @Injectable()
 export class SubloginsSettingsService extends TypeOrmCrudService<SubloginSettings> {
@@ -18,31 +19,6 @@ export class SubloginsSettingsService extends TypeOrmCrudService<SubloginSetting
 
   public async createBuilder(parsed: ParsedRequestParams, options: CrudRequestOptions, many: boolean = true, withDeleted: boolean = true) {
     const builder = await super.createBuilder(parsed, options, many, withDeleted);
-
-    if (!many) {
-      return builder;
-    }
-
-    const orderBys = builder.expressionMap.orderBys;
-    const direction = orderBys['instrumentRel.name'] ?? 'ASC';
-
-    builder.addSelect(`
-      CASE 
-        WHEN "SubloginSettings_instrument" LIKE "[%" THEN 1 
-        WHEN "SubloginSettings_instrument" LIKE "@%" THEN 2 
-        WHEN "SubloginSettings_instrument" LIKE "#%" THEN 3 
-        ELSE 4 
-      END
-    `, 'custom_order');
-
-    // We do not use addOrderBy because it does not allow to set object literal. So this won't work:
-    // builder.orderBy(our case);
-    // builder.addOrderBy(parsed, options.query);
-    // So we have to set it directly.
-    builder.expressionMap.orderBys = {
-      ['custom_order']: direction,
-      ...orderBys,
-    };
-    return builder;
+    return updateBuilderSort<SubloginSettings>(builder, many, 'instrumentRel.name', '`instrumentGroup`.`name`');
   }
 }
--- packages/liquidity-manager-backend/src/modules/view-instruments-specifications/view-instruments-specifications.service.ts
+++ packages/liquidity-manager-backend/src/modules/view-instruments-specifications/view-instruments-specifications.service.ts
@@ -6,6 +6,7 @@ import { AMTS_DB_NAME } from "../liquidity-app/liquidity-app.config";
 import { Repository } from "typeorm";
 import { CrudRequestOptions } from '@nestjsx/crud';
 import { ParsedRequestParams } from '@nestjsx/crud-request';
+import { updateBuilderSort } from '../../tools/instrumentSort';
 
 @Injectable()
 export class ViewInstrumentsSpecificationsService extends TypeOrmCrudService<ViewInstrumentsSpecification> {
@@ -18,31 +19,6 @@ export class ViewInstrumentsSpecificationsService extends TypeOrmCrudService<Vie
 
   public async createBuilder(parsed: ParsedRequestParams, options: CrudRequestOptions, many: boolean = true, withDeleted: boolean = true) {
     const builder = await super.createBuilder(parsed, options, many, withDeleted);
-
-    if (!many) {
-      return builder;
-    }
-
-    const orderBys = builder.expressionMap.orderBys;
-    const direction = orderBys['ViewInstrumentsSpecification.instrument'] ?? 'ASC';
-
-    builder.addSelect(`
-      CASE 
-        WHEN "ViewInstrumentsSpecification_instrument" LIKE "[%" THEN 1 
-        WHEN "ViewInstrumentsSpecification_instrument" LIKE "@%" THEN 2 
-        WHEN "ViewInstrumentsSpecification_instrument" LIKE "#%" THEN 3 
-        ELSE 4 
-      END
-    `, 'custom_order');
-
-    // We do not use addOrderBy because it does not allow to set object literal. So this won't work:
-    // builder.orderBy(our case);
-    // builder.addOrderBy(parsed, options.query);
-    // So we have to set it directly.
-    builder.expressionMap.orderBys = {
-      ['custom_order']: direction,
-      ...orderBys,
-    };
-    return builder;
+    return updateBuilderSort<ViewInstrumentsSpecification>(builder, many, 'ViewInstrumentsSpecification.instrument', '`ViewInstrumentsSpecification`.`instrument`');
   }
 }
