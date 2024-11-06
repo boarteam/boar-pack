6988491b20bc6d9b8ebad34d98f272f9930b4171
proxcentb <proxcentb@gmail.com>
2024-11-06 20:42:56 +0300

    ADM-208 apply sort for other entities

--- packages/liquidity-manager-backend/src/tools/instrumentSort.ts
+++ packages/liquidity-manager-backend/src/tools/instrumentSort.ts
@@ -1,21 +1,34 @@
 import { ObjectLiteral } from 'typeorm';
+import { SelectQueryBuilder } from 'typeorm/query-builder/SelectQueryBuilder';
 
-export function getCustomInstrumentSort(
-  originalSort: ObjectLiteral,
-  instrumentPath: string,
-  finalInstrumentPath?: string,
-): ObjectLiteral {
-  const sql = `
+export function updateBuilderSort<T extends ObjectLiteral>(
+    builder: SelectQueryBuilder<T>, 
+    many: boolean, 
+    orderBysInstName: string, 
+    caseSelectInstName: string,
+) {
+  if (!many) {
+    return builder;
+  }
+
+  builder.addSelect(`
     CASE 
-      WHEN ${finalInstrumentPath ?? instrumentPath} LIKE "[%" THEN 1 
-      WHEN ${finalInstrumentPath ?? instrumentPath} LIKE "@%" THEN 2 
-      WHEN ${finalInstrumentPath ?? instrumentPath} LIKE "#%" THEN 3 
+      WHEN ${caseSelectInstName} LIKE "[%" THEN 1 
+      WHEN ${caseSelectInstName} LIKE "@%" THEN 2 
+      WHEN ${caseSelectInstName} LIKE "#%" THEN 3 
       ELSE 4 
     END
-  `;
+  `, 'inst_custom_order');
 
-  return {
-    [sql]: originalSort[instrumentPath] ?? 'ASC',
-    ...originalSort,
+  // We do not use addOrderBy because it does not allow to set object literal. So this won't work:
+  // builder.orderBy(our case);
+  // builder.addOrderBy(parsed, options.query);
+  // So we have to set it directly.
+  const orderBys = builder.expressionMap.orderBys;
+  builder.expressionMap.orderBys = {
+    ['inst_custom_order']: orderBys[orderBysInstName] ?? 'ASC',
+    ...orderBys,
   };
+
+  return builder;
 }
