410d6a0165b3914185e3c616d1a083cbc15a6d23
Denis Volper <denisvolper@gmail.com>
2024-11-18 18:34:14 +0300

    Simplified audit logs table

--- packages/liquidity-manager-frontend/src/components/AuditLogs/AuditLogExplanation.tsx
+++ packages/liquidity-manager-frontend/src/components/AuditLogs/AuditLogExplanation.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { EventLog } from "@@api/generated";
-import { Card, Descriptions, Tag, Typography } from "antd";
+import { Card, Tag, Typography } from "antd";
 import {
   ClockCircleOutlined,
   ExclamationCircleOutlined,
@@ -10,15 +10,7 @@ import {
 } from "@ant-design/icons";
 import { UserAgentDisplay } from "@jifeon/boar-pack-users-frontend";
 
-const { Paragraph, Text, Title } = Typography;
-
-function formatJson(s: string): string {
-  try {
-    return JSON.stringify(JSON.parse(s), null, 2);
-  } catch (e) {
-    return s;
-  }
-}
+const { Paragraph, Title } = Typography;
 
 function renderUserAgentText(userAgent: string | null): React.ReactNode {
   return userAgent
@@ -26,66 +18,9 @@ function renderUserAgentText(userAgent: string | null): React.ReactNode {
       <pre>{userAgent}</pre>
     </>
     : 'without User-Agent header';
-
 }
 
 export const AuditLogExplanation: React.FC<{ record: EventLog }> = ({ record }) => {
-  const duration = record.duration !== null ? <Tag icon={<ClockCircleOutlined />}>{record.duration} ms</Tag> : 'unknown time ';
-
-  let statusCode: React.ReactNode;
-  if (record.statusCode === null) {
-    statusCode = <Tag>Unknown</Tag>;
-  } else if (record.statusCode >= 500) {
-    statusCode = <Tag color='red'>{record.statusCode}</Tag>;
-  } else if (record.statusCode >= 300) {
-    statusCode = <Tag color='orange'>{record.statusCode}</Tag>;
-  } else if (record.statusCode >= 200) {
-    statusCode = <Tag color='green'>{record.statusCode}</Tag>;
-  }
-
-  // get query params form url
-  let queryParamsEl: React.ReactNode = <>N/A<br /></>;
-  if (record.url) {
-    let url: URL;
-    try {
-      url = new URL(window.location.origin + record.url);
-    } catch (e) {
-      console.error(e);
-      url = new URL(window.location.origin);
-    }
-    const queryParams = Array.from(url.searchParams.entries());
-    if (queryParams.length) {
-      queryParamsEl = <Descriptions column={1} size='small' bordered={true} items={
-        queryParams.map(([key, value], i) => {
-          return {
-            key: i,
-            label: key,
-            children: key !== 's' ? <Text code>{value}</Text> : <pre style={{
-              fontSize: '0.8em',
-            }}>{formatJson(value)}</pre>
-          };
-        })
-      } />
-    }
-  }
-  const query = <Paragraph>
-    <Title level={5}>Query Parameters</Title>
-    {queryParamsEl}
-  </Paragraph>;
-
-  let payloadEl: React.ReactNode = <>N/A<br /></>;
-  if (record.payload) {
-    payloadEl = <pre>{JSON.stringify(record.payload, null, 2).replace(/\n/g, '\n')}</pre>;
-  }
-  const payload = <Paragraph>
-    <Title level={5}>Action Payload</Title>
-    {payloadEl}
-  </Paragraph>;
-
-  const actionEl = record.action === 'Access'
-    ? <>tried to access <Tag>{record.entity}</Tag>controller</>
-    : <>performed <Tag>{record.entity}.{record.action}</Tag>action</>;
-
   let logLevelEl = <Tag color={'warning'} icon={<WarningOutlined />}>{record.logLevel}</Tag>;
   if (record.logLevel === EventLog.logLevel.INFO) {
     logLevelEl = <Tag color={'blue'} icon={<InfoCircleOutlined />}>{record.logLevel}</Tag>;
@@ -100,19 +35,9 @@ export const AuditLogExplanation: React.FC<{ record: EventLog }> = ({ record }) 
     </>}
   >
     <Paragraph>
-      {record.userName} {actionEl} as result of the HTTP request<br />
-      <pre>
-        {record.method} {record.url}<br />
-      </pre>
-      {query}
-      {payload}
       <Title level={5}>Client</Title>
       Request was done from IP address <Tag>{record.ipAddress}</Tag><br />
       {renderUserAgentText(record.userAgent)}
     </Paragraph>
-    {(record.duration || record.statusCode) ? <Paragraph>
-      <Title level={5}>Response</Title>
-      The request took {duration}and returned status code {statusCode}<br />
-    </Paragraph> : null}
   </Card>;
 }
