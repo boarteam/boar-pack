6e51646212074fa34ad94a6ef42f824050ee33a3
Andrew Balakirev <balakirev.andrey@gmail.com>
2025-02-06 12:51:39 +0100

    feat(users-backend): Tokens auth guard and strategy

---
+++ packages/users-backend/src/tokens/tokens-auth.strategy.ts
@@ -0,0 +1,43 @@
+import { Strategy } from 'passport-http-bearer';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { TUser } from '../users';
+import { TOKENS_AUTH } from "./tokens.constants";
+import { TokensService } from "./tokens.service";
+import { BcryptService } from "../bcrypt";
+import Joi from "joi";
+
+@Injectable()
+export class TokensAuthStrategy extends PassportStrategy(Strategy, TOKENS_AUTH) {
+  private uuidValidationSchema = Joi.string().uuid({ version: ['uuidv4'] });
+
+  constructor(
+    private tokens: TokensService,
+    private bcryptService: BcryptService,
+  ) {
+    super();
+  }
+
+  async validate(token: string): Promise<TUser> {
+    const [id, hash] = token.split('.');
+
+    const { error } = this.uuidValidationSchema.validate(id);
+    if (error || !hash) {
+      throw new UnauthorizedException('Invalid token');
+    }
+
+    const tokenEntity = await this.tokens.findOne({
+      relations: ['user'],
+      where: {
+        id,
+      },
+    });
+
+    if (!tokenEntity || !await this.bcryptService.compare(hash, tokenEntity.hash)) {
+      throw new UnauthorizedException('Invalid token');
+    }
+
+    const { pass, ...user } = tokenEntity.user;
+    return user;
+  }
+}
