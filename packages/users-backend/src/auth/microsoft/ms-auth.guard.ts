938759f8e85ff12c3542b0f664f2625eee380e9d
Andrey Balakirev <balakirev.andrey@gmail.com>
2025-03-17 21:59:50 +0300

    feat(auth): Yandex auth (#49)
    * refactor(auth): reorganize authentication files into respective directories
    
    * feat(auth): implement Google and Microsoft authentication controllers
    
    * feat(auth): add Yandex authentication support with config and controller
    
    * fix(auth): remove unnecessary scope from Yandex authentication configuration
    
    * Publish
    
     - @boarteam/boar-pack-users-backend@6.0.0-alpha.1

--- packages/users-backend/src/auth/microsoft/ms-auth.guard.ts
+++ packages/users-backend/src/auth/microsoft/ms-auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { MS_AUTH } from './auth-strategies.constants';
+import { MS_AUTH } from '../auth-strategies.constants';
 
 @Injectable()
 export class MSAuthGuard extends AuthGuard(MS_AUTH) {
