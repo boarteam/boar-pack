664e9b2ddf31f4432a417d112f1072707cee32fb
Andrey Balakirev <balakirev.andrey@gmail.com>
2025-03-17 21:59:50 +0300

    feat(auth): Yandex auth (#49)
    * refactor(auth): reorganize authentication files into respective directories
    
    * feat(auth): implement Google and Microsoft authentication controllers
    
    * feat(auth): add Yandex authentication support with config and controller
    
    * fix(auth): remove unnecessary scope from Yandex authentication configuration
    
    * Publish
    
     - @boarteam/boar-pack-users-backend@6.0.0-alpha.1

---
+++ packages/users-backend/src/auth/google/google-auth.strategy.ts
@@ -0,0 +1,59 @@
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { PassportStrategy } from '@nestjs/passport';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthService } from '../auth.service';
+import { GOOGLE_AUTH } from '../auth-strategies.constants';
+import { GoogleAuthConfigService } from "./google-auth.config";
+
+@Injectable()
+export class GoogleAuthStrategy extends PassportStrategy(
+  Strategy,
+  GOOGLE_AUTH,
+) {
+  private readonly logger = new Logger(GoogleAuthStrategy.name);
+
+  constructor(
+    private authService: AuthService,
+    private googleAuthConfigService: GoogleAuthConfigService,
+  ) {
+    const config = googleAuthConfigService.config;
+    super({
+      clientID: config.clientId,
+      clientSecret: config.clientSecret,
+      callbackURL: config.callbackURL,
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: { emails: { value: string; verified: boolean }[] },
+    callback: VerifyCallback,
+  ): Promise<any> {
+    try {
+      const user = await this.authService.validateUserByEmail(
+        profile.emails[0].value,
+      );
+
+      if (!user) {
+        callback(new UnauthorizedException('User is not found'));
+        return;
+      }
+
+      callback(null, user);
+    } catch (e) {
+      this.logger.error(e, e.stack);
+      callback(
+        new InternalServerErrorException(
+          'Impossible to log in user via google',
+        ),
+      );
+    }
+  }
+}
