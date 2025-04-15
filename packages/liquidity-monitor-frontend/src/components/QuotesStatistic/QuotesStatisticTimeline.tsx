572cd6b0959d822516e598b5a647e96730a2ade3
Andrew Balakirev <balakirev.andrey@gmail.com>
2025-04-15 11:30:52 +0200

    refactor(QuotesStatisticTimeline): simplify component by removing unused state and logic

--- packages/liquidity-monitor-frontend/src/components/QuotesStatistic/QuotesStatisticTimeline.tsx
+++ packages/liquidity-monitor-frontend/src/components/QuotesStatistic/QuotesStatisticTimeline.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { PageLoading } from "@ant-design/pro-layout";
 import { QuotesStatisticDto, QuotesStatisticQueryDto } from "../../tools/api-client";
 import { Column, ColumnConfig } from "@ant-design/plots";
-import { Button } from "antd";
 import moment from "moment";
 import { quotesColors } from "./QuotesColors";
 import apiClient from "../../tools/api-client/apiClient";
@@ -24,35 +23,20 @@ export const QuotesStatisticTimeline: React.FC<TQuotesStatisticTimelineProps> = 
   providers,
 }) => {
   const [data, setData] = useState<QuotesStatisticDto[] | null>(null);
-  const sliderPosition = useMemo(() => [0, 1], []);
-  const [showFilterButton, setShowFilterButton] = useState(false);
 
   const { initialState } = useModel('@@initialState');
   const { navTheme } = initialState?.settings || {};
-
-  const chartRef = React.useRef(null);
 
   useEffect(() => {
     apiClient.quotesStatistics.getTimeline({
       startTime,
       endTime,
       timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-    }).then(setData).then(() => {
-      sliderPosition[0] = 0;
-      sliderPosition[1] = 1;
-      setShowFilterButton(false);
-    });
+    }).then(setData);
   }, [startTime, endTime]);
 
   if (!data || !providers) {
     return <PageLoading />;
-  }
-
-  const applySliderDates = () => {
-    const [start, end] = sliderPosition;
-    const startDate = data?.[Math.floor(start * (data.length - 1))]?.startTime;
-    const endDate = data?.[Math.floor(end * (data.length - 1))]?.endTime;
-    onDateRangeChange(startDate, endDate);
   }
 
   const config: ColumnConfig = {
@@ -60,7 +44,7 @@ export const QuotesStatisticTimeline: React.FC<TQuotesStatisticTimelineProps> = 
     xField: 'startTime',
     yField: 'records',
     colorField: (record: QuotesStatisticDto) => {
-      return providers[record.providerName]?.name
+      return providers[record.providerName]?.name || record.providerName;
     },
     stack: true,
     height: 300,
@@ -99,13 +83,7 @@ export const QuotesStatisticTimeline: React.FC<TQuotesStatisticTimelineProps> = 
     }
   };
 
-  return <>
-    <Column
-      {...config}
-      ref={chartRef}
-    />
-    {
-      showFilterButton && <Button onClick={applySliderDates}>Filter dates</Button> || null
-    }
-  </>
+  return <Column
+    {...config}
+  />;
 }
