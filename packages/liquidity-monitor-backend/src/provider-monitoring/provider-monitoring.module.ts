8f06e7e4e7ed1bfa7c5426bcb7a732776e29b650
Denis Volper <denisvolper@gmail.com>
2025-05-06 13:13:45 +0300

    refactor(liquidity monitor backend): PLA-26 Passing providers request function to the provider monitoring module

--- packages/liquidity-monitor-backend/src/provider-monitoring/provider-monitoring.module.ts
+++ packages/liquidity-monitor-backend/src/provider-monitoring/provider-monitoring.module.ts
@@ -1,17 +1,22 @@
-import { Module } from "@nestjs/common";
+import { DynamicModule, Module } from "@nestjs/common";
 import { ProviderMonitoringService } from "./provider-monitoring.service";
 import { TelegrafModule } from "@boarteam/boar-pack-users-backend";
 import { ProviderMonitoringController } from "./provider-monitoring.controller";
 import { SettingsModule } from "@boarteam/boar-pack-users-backend";
+import { FETCH_PROVIDERS } from "./provider-monitoring.constants";
 
 @Module({})
 export class ProviderMonitoringModule {
-  static forRoot(config: {
+  static forRootAsync(config: {
     dataSourceName: string,
-  }) {
+    useFactory: (...args: any[]) => () => Promise<any[]>,
+    inject?: any[],
+    imports?: any[],
+  }): DynamicModule {
     return {
       module: ProviderMonitoringModule,
       imports: [
+        ...(config.imports || []),
         TelegrafModule.register({
           withControllers: false,
           dataSourceName: config.dataSourceName,
@@ -26,6 +31,11 @@ export class ProviderMonitoringModule {
       ],
       providers: [
         ProviderMonitoringService,
+        {
+          provide: FETCH_PROVIDERS,
+          inject: config.inject,
+          useFactory: config.useFactory,
+        },
       ],
       exports: [
         ProviderMonitoringService,
