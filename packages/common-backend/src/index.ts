b0a1efee64b62f31f954c78bdb9f48ff5da2cfc5
Denis Volper <39730561+devdevdenis@users.noreply.github.com>
2025-05-07 12:38:29 +0300

    (#58)
    * refactor(quotes-statistic): use ScheduleModule from boar-pack-common-backend
    
    * refactor(schedule-service): check swagger during onApplicationBootstrap lifecycle hook

--- packages/common-backend/src/index.ts
+++ packages/common-backend/src/index.ts
@@ -1,5 +1,6 @@
 export * from './modules/scrypt/scrypt.module';
 export * from './modules/scrypt/scrypt.service';
+export * from './modules/schedule/schedule.module';
 export * from './modules/cluster';
 export * from './modules/websockets';
 export * as Tools from './tools';
