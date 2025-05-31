098dfab43089861c67da42e8bf51ad898d50ca0e
Denis Volper <denisvolper@gmail.com>
2025-05-31 12:36:34 +0300

    feature(liquidity-monitor): ADM-294 add Instruments History module and availability report endpoint

---
+++ packages/liquidity-monitor-frontend/src/tools/api-client/generated/models/InstrumentsHistoryQueryDto.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type InstrumentsHistoryQueryDto = {
+    sort?: string;
+    start?: string;
+    end?: string;
+    groupId?: string;
+    platformId?: string;
+};
+
---
+++ packages/liquidity-monitor-frontend/src/tools/api-client/generated/models/InstrumentsHistoryResponseDto.ts
@@ -0,0 +1,9 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { InstrumentsHistoryDto } from './InstrumentsHistoryDto';
+export type InstrumentsHistoryResponseDto = {
+    data: Array<InstrumentsHistoryDto>;
+};
+
