05a14a7aac2a0d81710a61eb03346300f53f1cd2
Andrew Balakirev <balakirev.andrey@gmail.com>
2025-06-05 13:45:46 +0200

    refactor(entity): change date columns to use timestamptz type
    BREAKING CHANGE: All dates will be handled differently from previous behavior

--- packages/users-backend/src/tokens/entities/token.entity.ts
+++ packages/users-backend/src/tokens/entities/token.entity.ts
@@ -18,9 +18,9 @@ export class Token {
   @ManyToOne(() => User)
   user: User;
 
-  @CreateDateColumn({ name: 'created_at' })
+  @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
   createdAt: Date;
 
-  @UpdateDateColumn({ name: 'updated_at' })
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamptz' })
   updatedAt: Date;
 }
