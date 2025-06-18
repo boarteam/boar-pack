9ffb26beda121c1355ba822d0eed40acfc8a3817
Andrew Balakirev <balakirev.andrey@gmail.com>
2025-06-18 22:12:16 +0200

    refactor(useAvailabilityReportColumns): simplify column titles by removing tooltips

--- packages/liquidity-monitor-frontend/src/components/AvailabilityReport/useAvailabilityReportColumns.tsx
+++ packages/liquidity-monitor-frontend/src/components/AvailabilityReport/useAvailabilityReportColumns.tsx
@@ -1,4 +1,3 @@
-import { Tooltip } from "antd";
 import { ProColumns } from "@ant-design/pro-components";
 import { InstrumentsHistoryDto } from "../../tools/api-client";
 
@@ -7,42 +6,26 @@ export const useAvailabilityReportColumns = (columnFilters: {
 }): ProColumns<InstrumentsHistoryDto>[] => {
   return [
     {
-      title: (
-        <Tooltip title='Instrument'>
-          Trading instrument name
-        </Tooltip>
-      ),
+      title: 'Symbol',
       dataIndex: "symbolName",
       sorter: true
     },
     {
-      title: (
-        <Tooltip title='Group'>
-          Instruments group
-        </Tooltip>
-      ),
+      title: 'Instruments group',
       dataIndex: "groupName",
       ...(columnFilters?.['groupName'] && {
         filters: columnFilters?.['groupName'],
       })
     },
     {
-      title: (
-        <Tooltip title='Platform'>
-          Trading platform
-        </Tooltip>
-      ),
+      title: 'Trading platform',
       dataIndex: "platformName",
       ...(columnFilters?.['platformName'] && {
         filters: columnFilters?.['platformName'],
       })
     },
     {
-      title: (
-        <Tooltip title='Availability %'>
-          Instrument availability percentage
-        </Tooltip>
-      ),
+      title: 'Availability',
       dataIndex: "availabilityPercent",
       renderText: (text: string) => {
         return `${text}%`;
@@ -50,51 +33,27 @@ export const useAvailabilityReportColumns = (columnFilters: {
       sorter: true
     },
     {
-      title: (
-        <Tooltip title='Scheduled Off (hh:mm)'>
-          Scheduled quotes downtime duration
-        </Tooltip>
-      ),
+      title: 'Off quotes',
       dataIndex: "scheduledOffQuotesDuration",
     },
     {
-      title: (
-        <Tooltip title='Downtime (hh:mm)'>
-          Total downtime duration
-        </Tooltip>
-      ),
+      title: 'Total downtime',
       dataIndex: "downtimeDuration",
     },
     {
-      title: (
-        <Tooltip title='Incidents'>
-          Number of downtime incidents
-        </Tooltip>
-      ),
+      title: '№ incidents',
       dataIndex: "incidentsCount",
     },
     {
-      title: (
-        <Tooltip title='Max Incident (hh:mm)'>
-          Maximum duration of a single incident
-        </Tooltip>
-      ),
+      title: 'Max incident duration',
       dataIndex: "maxIncidentDuration",
     },
     {
-      title: (
-        <Tooltip title='Avg Incident (hh:mm)'>
-          Average incident duration
-        </Tooltip>
-      ),
+      title: 'Avg incident duration',
       dataIndex: "meanIncidentDuration",
     },
     {
-      title: (
-        <Tooltip title='Reasons'>
-          Downtime reasons with duration (hh:mm)
-        </Tooltip>
-      ),
+      title: 'Downtime reasons',
       dataIndex: "reason",
     },
   ];
