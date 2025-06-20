596ed87daa93863cb7742d9723ab8d9f33b3c275
Denis Volper <denisvolper@gmail.com>
2025-06-20 17:31:03 +0300

    feat(provider-monitoring): ADM-303 add ProvidersProblematicPeriod entity and update monitoring logic
    BREAKING CHANGE: ProviderMonitoringModule import changes

--- packages/liquidity-monitor-backend/src/provider-monitoring/provider-monitoring.module.ts
+++ packages/liquidity-monitor-backend/src/provider-monitoring/provider-monitoring.module.ts
@@ -1,11 +1,13 @@
 import { DynamicModule, InjectionToken, Module, OptionalFactoryDependency } from "@nestjs/common";
 import { ProviderMonitoringService } from "./provider-monitoring.service";
-import { TelegrafModule } from "@boarteam/boar-pack-users-backend";
+import { SettingsModule, SettingsService, TelegrafModule, TelegrafService } from "@boarteam/boar-pack-users-backend";
 import { ProviderMonitoringController } from "./provider-monitoring.controller";
-import { SettingsModule } from "@boarteam/boar-pack-users-backend";
 import { FETCH_PROVIDERS } from "./provider-monitoring.constants";
 import { Type } from "@nestjs/common/interfaces/type.interface";
 import { ForwardReference } from "@nestjs/common/interfaces/modules/forward-reference.interface";
+import { getDataSourceToken, TypeOrmModule } from "@nestjs/typeorm";
+import { ProvidersProblematicPeriod } from "./entities/providers-problematic-period.entity";
+import { DataSource } from "typeorm";
 
 // All providers should have at least these fields
 export type TProvider = {
@@ -16,9 +18,9 @@ export type TProvider = {
 
 @Module({})
 export class ProviderMonitoringModule {
-  static forRootAsync(config: {
+  static forRootAsync<TLocalProvider extends TProvider, TLocalService>(config: {
     dataSourceName: string,
-    useFactory: <TProvider, TProviderService>(service: TProviderService) => () => Promise<TProvider[]>,
+    fetchProviders: (service: TLocalService) => Promise<TLocalProvider[]>,
     inject?: (InjectionToken | OptionalFactoryDependency)[],
     imports?: Array<Type<any> | DynamicModule | Promise<DynamicModule> | ForwardReference>,
   }): DynamicModule {
@@ -26,6 +28,7 @@ export class ProviderMonitoringModule {
       module: ProviderMonitoringModule,
       imports: [
         ...(Array.isArray(config.imports) ? config.imports : []),
+        TypeOrmModule.forFeature([ProvidersProblematicPeriod], config.dataSourceName),
         TelegrafModule.register({
           withControllers: false,
           dataSourceName: config.dataSourceName,
@@ -39,11 +42,34 @@ export class ProviderMonitoringModule {
         ProviderMonitoringController
       ],
       providers: [
-        ProviderMonitoringService,
         {
           provide: FETCH_PROVIDERS,
           inject: config.inject,
-          useFactory: config.useFactory,
+          useFactory: (providersService) => {
+            return () => config.fetchProviders(providersService);
+          },
+        },
+        {
+          provide: ProviderMonitoringService,
+          inject: [
+            getDataSourceToken(config.dataSourceName),
+            TelegrafService,
+            SettingsService,
+            FETCH_PROVIDERS,
+          ],
+          useFactory: (
+            dataSource: DataSource,
+            telegrafService: TelegrafService,
+            settingsService: SettingsService,
+            fetchProviders: () => Promise<TLocalProvider[]>,
+          ) => {
+            return new ProviderMonitoringService(
+              dataSource,
+              telegrafService,
+              settingsService,
+              fetchProviders,
+            );
+          },
         },
       ],
       exports: [
