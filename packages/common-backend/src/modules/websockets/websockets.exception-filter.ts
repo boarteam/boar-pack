53910b68213c6487b96592b7a98652ed1a58980b
Andrew Balakirev <balakirev.andrey@gmail.com>
2025-07-21 13:35:49 +0200

    feat: enhance error handling in Websockets with optional details

--- packages/common-backend/src/modules/websockets/websockets.exception-filter.ts
+++ packages/common-backend/src/modules/websockets/websockets.exception-filter.ts
@@ -1,15 +1,16 @@
 import { Catch, ArgumentsHost } from '@nestjs/common';
-import { BaseWsExceptionFilter } from '@nestjs/websockets';
+import { BaseWsExceptionFilter, WsException } from '@nestjs/websockets';
 import { WebsocketsErrorEventDto } from "./dto/websockets.dto";
 
 @Catch()
 export class WebsocketsExceptionFilter extends BaseWsExceptionFilter {
-  catch(exception: Error, host: ArgumentsHost) {
+  catch(exception: Error | WsException, host: ArgumentsHost) {
     const client = host.switchToWs().getClient();
     client.send(JSON.stringify({
       event: 'error',
       data: {
         message: exception.message,
+        details: exception instanceof WsException ? (exception.getError() as any)?.details : undefined,
       },
     } as WebsocketsErrorEventDto));
 
