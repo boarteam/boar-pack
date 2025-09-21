366469831066452c072bc89dc7584f757f68ab03
Andrew Balakirev <balakirev.andrey@gmail.com>
2025-09-22 00:41:51 +0200

    feat(users-backend): enforce CASL policy in `getMany` method of `AuditLogsController`
    - Add CASL-based access control to filter audit logs based on user's permissions.
    - Integrate `CaslAbilityFactory` for generating user-specific access abilities.
    - Update `getMany` to restrict visible tables to those authorized by the current user's policies.

--- packages/users-backend/src/audit-logs/audit-logs.controller.ts
+++ packages/users-backend/src/audit-logs/audit-logs.controller.ts
@@ -1,10 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Req, UnauthorizedException } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { Crud, CrudController } from '@dataui/crud';
+import { Crud, CrudController, CrudRequest, Override, ParsedRequest } from '@dataui/crud';
 import { CheckPolicies } from "@boarteam/boar-pack-users-backend";
 import { AuditLogsService } from './audit-logs.service';
 import { AuditLog } from './entities/audit-log.entity';
 import { ViewAuditLogsPolicy } from "./policies/view-audit-logs.policy";
+import { Action, CaslAbilityFactory, Subjects } from "../casl";
+import { Request } from "express";
+import { TUser } from "../users";
 
 @Crud({
   model: {
@@ -35,5 +38,37 @@ import { ViewAuditLogsPolicy } from "./policies/view-audit-logs.policy";
 export class AuditLogsController implements CrudController<AuditLog>{
   constructor(
     readonly service: AuditLogsService,
+    private readonly caslAbilityFactory: CaslAbilityFactory,
   ) {}
+
+  get base(): CrudController<AuditLog> {
+    return this;
+  }
+
+  @Override('getManyBase')
+  async getMany(
+    @Req() request: Request,
+    @ParsedRequest() req: CrudRequest<TUser>,
+  ) {
+    const user = request.user;
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    const ability = await this.caslAbilityFactory.createForUser(user);
+    if (ability.cannot(Action.Manage, 'all')) {
+      const tablesNames = this.service.repo.manager.connection.entityMetadatas
+        .filter(meta => meta.target instanceof Function && ability.can(Action.Manage, meta.target as Subjects))
+        .map(meta => meta.tableName);
+
+      req.parsed.search = {
+        $and: [
+          req.parsed.search,
+          { tableName: { $in: tablesNames } },
+        ],
+      }
+    }
+
+    return this.base.getManyBase!(req);
+  }
 }
